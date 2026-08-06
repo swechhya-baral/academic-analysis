a41b56343c5804b27811ea7ea16395a12ac32bb9
swechhya-baral <swechhyabaral@gmail.com>
2026-08-06 16:46:46 +0545

    Improved form styling for login, add grade and add attendance pages

--- academics/forms.py
+++ academics/forms.py
@@ -5,11 +5,19 @@ class GradeForm(forms.ModelForm):
     class Meta:
         model = Grade
         fields = ['enrollment', 'assessment_type', 'score', 'max_score']
+        widgets = {
+            'enrollment': forms.Select(attrs={'class': 'form-select'}),
+            'assessment_type': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g. Midterm'}),
+            'score': forms.NumberInput(attrs={'class': 'form-control'}),
+            'max_score': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
 
 class AttendanceForm(forms.ModelForm):
     class Meta:
         model = Attendance
         fields = ['enrollment', 'date', 'present']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
+            'enrollment': forms.Select(attrs={'class': 'form-select'}),
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'present': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'academics'
+    'academics',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
